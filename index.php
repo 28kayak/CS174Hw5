d339cccc0b0ade5a0faf5ba7d212f6b210ce6bdd
28kayak <kaya.ota@sjsu.edu>
2016-05-07 22:17:12 -0700

    update

--- index.php
+++ index.php
@@ -1,5 +1,30 @@
 <?php
 
+$lang = $_SERVER["HTTP_ACCEPT_LANGUAGE"];
+
+$temp = explode(";", $lang);
+$lang = explode(",", $temp[0]);
+$message = "lang[0] = ".$lang[0];
+echo "<script type='text/javascript'>alert('$message');</script>";
+
+if ($lang[0] == "en-US") 
+{
+	# code...
+	include("index.html");
+}
+else if ($lang[0] == "ja")
+{
+	# code...
+	include("index-jp.html");
+
+}
+else
+{
+	include();
+}
+
+
+
 
 
 
