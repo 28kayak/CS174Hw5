6f8a4b10c5dfc86c7d8dbad441394358acda6d62
28kayak <kaya.ota@sjsu.edu>
2016-05-14 00:22:40 -0700

    success save specials

--- controllers/save.php
+++ controllers/save.php
@@ -1,6 +1,3 @@
-
-
-
 <?php
 /*After save button clicked
 2 effect will happen:
@@ -9,11 +6,20 @@
 
 */
 
-$menu_specials = $_REQUEST['menu'];
-$db = mysqli_connect("localhost","root","root","Hw5");
+ //DataBase Infomartion
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "Hw5";
 
-$query = "insert into MENU values (\"".$menu_specials."\")";
-		//echo $query;
+//get menu-specials from form
+$menu_specials = $_REQUEST['menu'];
+
+//create DB connection
+$db = new mysqli($servername, $username, $password,$dbname);
+//insert into MENU(specials) values ("the current menu activity4")
+$query = "insert into MENU(specials) values (\"".$menu_specials."\")";
+echo "<br> Query = ".$query."<br>";
 $result = mysqli_query($db, $query);
 if ($result == true)  //??????
 {
